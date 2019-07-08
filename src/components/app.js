a3cabc99c82fc1b05f27645ddd1e9d5d40acad0f
ScarletGamerArtist <alannabotscharow@gmail.com>
2019-07-08 11:32:56 -0600

    HELP

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Alanna Botscharow Portfolio</h1>
-        <div>
-          {moment().format('MMM Do YYYY, h:mm:ss a')}
-        </div>
+        <div>{moment().format('MMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />       
       </div>
     );
   }
